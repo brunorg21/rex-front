b1e32aa02d1ede075410b5fa1a0b435abaea2186
brunorg21 <rbrunog21@gmail.com>
2024-02-27 15:14:20 -0300

    refactor: change withCredentials on login

--- src/api/user-service.ts
+++ src/api/user-service.ts
@@ -1,13 +1,10 @@
 import { SignInFormData } from "@/app/auth/sign-in/page";
 import { SignUpFormData } from "@/app/auth/sign-up/page";
 import { api } from "@/lib/axios-client";
-import { CookieValueTypes } from "cookies-next";
 
 export const useUserService = () => {
   async function login(data: SignInFormData) {
-    return await api.post("/user/login", data, {
-      withCredentials: true,
-    });
+    return await api.post("/user/login", data);
   }
 
   async function createUser(data: SignUpFormData) {

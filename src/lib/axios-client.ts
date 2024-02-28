a93e0483deca4d5578b464e0318ce49c6aa27c23
brunorg21 <rbrunog21@gmail.com>
2024-02-28 12:54:26 -0300

    refactor: change axios client configuration

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.put["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
-
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
   headers: {

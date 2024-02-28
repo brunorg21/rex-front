5cd6b0ad67a6202d6d65e158bf79595115f6c955
brunorg21 <rbrunog21@gmail.com>
2024-02-28 09:32:22 -0300

    refactor: api url

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -5,5 +5,5 @@ axios.defaults.headers.put["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
 });
